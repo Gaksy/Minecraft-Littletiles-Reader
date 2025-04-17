73f1daf0016ea3f0ea02d799e812f6743f4fd83d
Gaksy <gaksys@outlook.com>
2025-04-17 09:47:48 +0800

    Add Little tiles Exception

---
+++ Galib/Exception/LittleTilesException.cpp
@@ -0,0 +1,40 @@
+/*
+ * This Source Code Form is subject to the terms of the GNU Lesser General Public License,
+ * v. 3.0. If a copy of the LGPL was not distributed with this file, You can obtain one at
+ * https://www.gnu.org/licenses/lgpl-3.0.html.
+ *
+ * Copyright (c) 2024 Gaksy (Fuhongren)
+ *
+ * Author: Gaksy
+ * Date Created: 4/17/2025
+ *
+ * Additional Terms:
+ * For commercial use of this software, you must obtain separate authorization from the original author.
+ * Please contact Gaksy at gaksys@outlook.com to request commercial use authorization.
+ */
+
+#include "Exception/GalibExceptionBasic.h"
+#include "Exception/LittleTilesException.h"
+
+constexpr static const char *const STD_ERROR_MESSAGE[] = {
+    "LittleTiles Box angle enum arg is invalid."
+};
+
+constexpr static const char *const STD_ERROR_CODE[] = {
+    "lt_unknow_angle"
+};
+
+constexpr static const char *const EXCEPTION_NAME = "LittleTiles Exception";
+
+GALIB exception::LittleTilesException::LittleTilesException(
+    const LittleTilesErrorCode &kErrorCode,
+    const char *kPErrorMessage,
+    const char *kPErrorSender
+): GalibExceptionBasic<LittleTilesErrorCodeType>(
+    static_cast<LittleTilesErrorCodeType>(kErrorCode),
+    kPErrorMessage,
+    kPErrorSender,
+    STD_ERROR_CODE[static_cast<LittleTilesErrorCodeType>(kErrorCode) - 1],
+    STD_ERROR_MESSAGE[static_cast<LittleTilesErrorCodeType>(kErrorCode) - 1],
+    EXCEPTION_NAME
+) { }
---
+++ Galib/include/Exception/LittleTilesException.h
@@ -0,0 +1,41 @@
+/*
+ * This Source Code Form is subject to the terms of the GNU Lesser General Public License,
+ * v. 3.0. If a copy of the LGPL was not distributed with this file, You can obtain one at
+ * https://www.gnu.org/licenses/lgpl-3.0.html.
+ *
+ * Copyright (c) 2024 Gaksy (Fuhongren)
+ *
+ * Author: Gaksy
+ * Date Created: 4/17/2025
+ *
+ * Additional Terms:
+ * For commercial use of this software, you must obtain separate authorization from the original author.
+ * Please contact Gaksy at gaksys@outlook.com to request commercial use authorization.
+ */
+#ifndef GALIB_INCLUDE_EXCEPTION_LITTLETILESEXCEPTION_H
+#define GALIB_INCLUDE_EXCEPTION_LITTLETILESEXCEPTION_H
+
+#include <cstdint>
+
+#include "GalibNamespaceDef.h"
+#include "Exception/GalibExceptionBasic.h"
+
+namespace galib::exception {
+    using LittleTilesErrorCodeType = GALIB_STD uint8_t;
+
+    enum class LittleTilesErrorCode: LittleTilesErrorCodeType{
+        lt_unknow_angle = 1
+    };
+
+    class LittleTilesException : public GALIB exception::GalibExceptionBasic<LittleTilesErrorCodeType>{
+    public:
+        explicit LittleTilesException(
+            const LittleTilesErrorCode &kErrorCode,
+            const char *kPErrorMessage = nullptr,
+            const char *kPErrorSender = nullptr
+        );
+    };
+
+}
+
+#endif //GALIB_INCLUDE_EXCEPTION_LITTLETILESEXCEPTION_H
