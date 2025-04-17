7f46ae741aae84349ffe41bbcf049f28005ec719
Gaksy <gaksys@outlook.com>
2025-04-17 14:56:01 +0800

    update LittleTiles

--- Galib/Minecraft/LittleTiles.cpp
+++ Galib/Minecraft/LittleTiles.cpp
@@ -14,9 +14,157 @@
  */
 
 #include "Minecraft/LittleTiles.h"
+#include "Exception/LittleTilesException.h"
+#include "Exception/MinecraftException.h"
 #include "GalibNamespaceDef.h"
 
-GALIB minecraft::littletiles::ChunkTiles::ChunkTiles():
+using GALIB minecraft::littletiles::GridType;
+using GALIB minecraft::littletiles::ChunkTileEntities;
+using GALIB minecraft::littletiles::BlockTileEntities;
+
+using GALIB minecraft::BlockCoordinate;
+using GALIB minecraft::ChunkCoordinate;
+
+using GALIB exception::MinecraftException;
+using GALIB exception::MinecraftErrorCode;
+using GALIB exception::LittleTilesException;
+using GALIB exception::LittleTilesErrorCode;
+
+
+
+BlockTileEntities::BlockTileEntities():
+    block_coordinate_({0, 0, 0}),
+    grid_(0)
+{ }
+
+void BlockTileEntities::readBlockTileNBT(const nbt::tag_compound &kBlockTilesNBT) {
+    // check block tiles root is not empty
+    if (!kBlockTilesNBT.size()) {
+        throw LittleTilesException(LittleTilesErrorCode::lt_tage_not_exist, "The block tile data does not exist.", "BlockTiles");
+    }
+
+    try {
+        // Get block coord
+        BlockCoordinate block_coord = {
+            kBlockTilesNBT.at("x").as<GALIB_NBT tag_int>().get(),
+            kBlockTilesNBT.at("y").as<GALIB_NBT tag_int>().get(),
+            kBlockTilesNBT.at("z").as<GALIB_NBT tag_int>().get()
+        };
+
+        // By default
+        GridType grid_type = 16;
+
+        if (kBlockTilesNBT.has_key("grid")) {
+            grid_type = kBlockTilesNBT.at("grid").as<GALIB_NBT tag_int>().get();
+        }
+
+        // Get ID
+        GALIB_STD string little_tiles_id = kBlockTilesNBT.at("id").as<GALIB_NBT tag_string>();
+
+        // Get tiles list
+        GALIB_NBT tag_list tiles = kBlockTilesNBT.at("content").at("tiles").as<nbt::tag_list>();
+
+        // Process block tiles
+        GALIB_STD string block_id;
+        GALIB_NBT tag_compound* p_boxes;
+        container box_tile_enities_map;
+
+        for (GALIB_NBT tag_list::iterator it = tiles.begin(); it != tiles.cend(); ++it) {
+            p_boxes = &it->as<GALIB_NBT tag_compound>();   // Get boxes
+            block_id = p_boxes->at("block").as<GALIB_NBT tag_string>().get(); // Get block id
+
+            // is haved block_id
+            if(box_tile_enities_map.find(block_id) != box_tile_enities_map.end()) {
+                continue;
+            }
+
+            // Get data
+            BoxTileEnities box_tile_enities;
+        }
+    }
+    catch (...) {
+        throw LittleTilesException(LittleTilesErrorCode::lt_tage_not_exist, "Some tag not exist.", "BlockTiles");
+    }
+}
+
+const BlockCoordinate & BlockTileEntities::getBlockCoordinate() const {
+    return block_coordinate_;
+}
+
+const GridType & BlockTileEntities::getGridType() const {
+    return grid_;
+}
+
+const std::string & BlockTileEntities::getLittleTilesID() const {
+    return little_tiles_id_;
+}
+
+BlockTileEntities::const_iterator BlockTileEntities::cbegin() const {
+    return box_tile_enities_map_.cbegin();
+}
+
+BlockTileEntities::const_iterator BlockTileEntities::cend() const {
+    return box_tile_enities_map_.cend();
+}
+
+GALIB minecraft::littletiles::ChunkTileEntities::ChunkTileEntities():
 	chunk_coordinate_({})
 { }
+
+void ChunkTileEntities::ReadChunk(
+    const AnvilReader::ChunkDataReference &kChunkDataReference
+) {
+    // Check chunk root is not empty
+    if (!kChunkDataReference.p_chunk_root) {
+        throw MinecraftException(
+            MinecraftErrorCode::mc_nbt_empty,
+            "The chunk NBT root node is empty.",
+            "ChunkTiles"
+        );
+    }
+
+    // Chenk TileEnities is exist
+    if (!kChunkDataReference.p_chunk_root->has_key("TileEnities")) {
+        throw LittleTilesException(LittleTilesErrorCode::lt_tage_not_exist, "The NBT \"TileEnities\" tag does not exist", "ChunkTiles");
+    }
+
+    // Get TileEnities
+    const GALIB_NBT tag_list& tiles_entities = kChunkDataReference.p_chunk_root->at("TileEnities").as<GALIB_NBT tag_list>();
+    container block_tile_entities;
+
+    // Decode...
+    for(GALIB_NBT tag_list::const_iterator it = tiles_entities.cbegin(); it != tiles_entities.cend(); ++it) {
+        try {
+            BlockTileEntities block_tiles;
+            block_tiles.readBlockTileNBT(it->as<GALIB_NBT tag_compound>);
+            block_tile_entities.push_back(block_tiles);
+        }
+        catch (...) { }
+    }
+
+    // DONE!!
+    chunk_coordinate_ = kChunkDataReference.chunk_info.chunk_coord;
+    block_tile_entities_.swap(block_tile_entities);
+}
+
+const ChunkCoordinate & ChunkTileEntities::GetChunkCoordinate() const {
+    return chunk_coordinate_;
+}
+
+ChunkTileEntities::const_iterator ChunkTileEntities::cbegin() const {
+    return block_tile_entities_.cbegin();
+}
+
+ChunkTileEntities::const_iterator ChunkTileEntities::cend() const {
+    return block_tile_entities_.cend();
+}
+
+void ChunkTileEntities::Clear() {
+    chunk_coordinate_ = {0, 0};
+    block_tile_entities_.clear();
+}
+
+bool ChunkTileEntities::IsEmpty() const {
+    return block_tile_entities_.empty();
+}
 
--- Galib/include/Minecraft/LittleTiles.h
+++ Galib/include/Minecraft/LittleTiles.h
@@ -26,121 +26,132 @@
 #include "Exception/LittleTilesException.h"
 
 namespace galib::minecraft::littletiles{
-    class BlockTiles {
-    public:
-
+    enum class AngleOffsetID: GALIB_STD uint8_t {
+        EUN = 0, // East Up North
+        EUS = 1, // East Up South
+        EDN = 2, // East Down North
+        EDS = 3, // East Down South
+        WUN = 4, // West Up North
+        WUS = 5, // West Up South
+        WDN = 6, // West Down North
+        WDS = 7  // West Down South
     };
 
-    class ChunkTiles {
+    struct AngleOffset {
+        bool x_enable   { false };
+        bool y_enable   { false };
+        bool z_enable   { false };
+
+        OffsetType x_offset { 0 };
+        OffsetType y_offset { 0 };
+        OffsetType z_offset { 0 };
+
+        GALIB_NODISCARD bool hasAnyEnable()const {
+            return x_enable || y_enable || z_enable;
+        }
+    };
+
+    struct Flipped {
+        bool down       { false };
+        bool up         { false };
+        bool north      { false };
+        bool south      { false };
+        bool west       { false };
+        bool east       { false };
+
+        GALIB_NODISCARD bool hasAnyEnable()const {
+            return down || up || north || south || west || east;
+        }
+    };
+
+    struct TileEntity {
+        AngleOffset offset_data[8];
+        Flipped flipped_data;
+        LittleTilesCoord pos_1 {0, 0, 0};
+        LittleTilesCoord pos_2 {0, 0, 0};
+
+        GALIB_NODISCARD bool hasAnyOffsetEnable()const {
+            return GALIB_STD any_of(
+                GALIB_STD begin(offset_data),
+                GALIB_STD end(offset_data),
+                [](const AngleOffset& data){return data.hasAnyEnable();}
+            );
+        }
+
+        GALIB_NODISCARD LittleTilesCoord applyAngleOffset(const AngleOffsetID angle_id)const {
+            LittleTilesCoord angle_coord = getVertices(angle_id);
+            AngleOffset angle_offset = offset_data[static_cast<size_t>(angle_id)];
+            // If unoffset, then offset is 0
+            angle_coord.x += angle_offset.x_offset;
+            angle_coord.y += angle_offset.y_offset;
+            angle_coord.z += angle_offset.z_offset;
+            return angle_coord;
+        }
+
+        GALIB_NODISCARD LittleTilesCoord getVertices(const AngleOffsetID angle_id)const {
+            switch (angle_id) {
+                case AngleOffsetID::WDS:
+                    return {pos_1.x, pos_1.y, pos_2.z};
+                case AngleOffsetID::WDN:
+                    return {pos_1.x, pos_1.y, pos_1.z};
+                case AngleOffsetID::EDN:
+                    return {pos_2.x, pos_1.y, pos_1.z};
+                case AngleOffsetID::EDS:
+                    return {pos_2.x, pos_1.y, pos_2.z};
+                case AngleOffsetID::WUN:
+                    return {pos_1.x, pos_2.y, pos_1.z};
+                case AngleOffsetID::WUS:
+                    return {pos_1.x, pos_2.y, pos_2.z};
+                case AngleOffsetID::EUS:
+                    return {pos_2.x, pos_2.y, pos_2.z};
+                case AngleOffsetID::EUN:
+                    return {pos_2.x, pos_2.y, pos_1.z};
+                default:
+                    throw exception::LittleTilesException(exception::LittleTilesErrorCode::lt_unknow_angle);
+            }
+        }
+    };
+
+    using BoxTileEnities = GALIB_STD vector<TileEntity>;
+
+    class BlockTileEntities {
     public:
-        enum class AngleOffsetID: GALIB_STD uint8_t {
-            EUN = 0, // East Up North
-            EUS = 1, // East Up South
-            EDN = 2, // East Down North
-            EDS = 3, // East Down South
-            WUN = 4, // West Up North
-            WUS = 5, // West Up South
-            WDN = 6, // West Down North
-            WDS = 7  // West Down South
-        };
-
-        struct AngleOffset {
-            bool x_enable   { false };
-            bool y_enable   { false };
-            bool z_enable   { false };
-
-            OffsetType x_offset { 0 };
-            OffsetType y_offset { 0 };
-            OffsetType z_offset { 0 };
-
-            GALIB_NODISCARD bool hasAnyEnable()const {
-                return x_enable || y_enable || z_enable;
-            }
-        };
-
-        struct Flipped {
-            bool down       { false };
-            bool up         { false };
-            bool north      { false };
-            bool south      { false };
-            bool west       { false };
-            bool east       { false };
-
-            GALIB_NODISCARD bool hasAnyEnable()const {
-                return down || up || north || south || west || east;
-            }
-        };
-
-        struct Box {
-            AngleOffset offset_data[8];
-            Flipped flipped_data;
-            LittleTilesCoord pos_1 {0, 0, 0};
-            LittleTilesCoord pos_2 {0, 0, 0};
-
-            GALIB_NODISCARD bool hasAnyOffsetEnable()const {
-                return GALIB_STD any_of(
-                    GALIB_STD begin(offset_data),
-                    GALIB_STD end(offset_data),
-                    [](const AngleOffset& data){return data.hasAnyEnable();}
-                );
-            }
-
-            GALIB_NODISCARD LittleTilesCoord applyAngleOffset(const AngleOffsetID angle_id)const {
-                LittleTilesCoord angle_coord = getVertices(angle_id);
-                AngleOffset angle_offset = offset_data[static_cast<size_t>(angle_id)];
-                // If unoffset, then offset is 0
-                angle_coord.x += angle_offset.x_offset;
-                angle_coord.y += angle_offset.y_offset;
-                angle_coord.z += angle_offset.z_offset;
-                return angle_coord;
-            }
-
-            GALIB_NODISCARD LittleTilesCoord getVertices(const AngleOffsetID angle_id)const {
-                switch (angle_id) {
-                    case AngleOffsetID::WDS:
-                        return {pos_1.x, pos_1.y, pos_2.z};
-                    case AngleOffsetID::WDN:
-                        return {pos_1.x, pos_1.y, pos_1.z};
-                    case AngleOffsetID::EDN:
-                        return {pos_2.x, pos_1.y, pos_1.z};
-                    case AngleOffsetID::EDS:
-                        return {pos_2.x, pos_1.y, pos_2.z};
-                    case AngleOffsetID::WUN:
-                        return {pos_1.x, pos_2.y, pos_1.z};
-                    case AngleOffsetID::WUS:
-                        return {pos_1.x, pos_2.y, pos_2.z};
-                    case AngleOffsetID::EUS:
-                        return {pos_2.x, pos_2.y, pos_2.z};
-                    case AngleOffsetID::EUN:
-                        return {pos_2.x, pos_2.y, pos_1.z};
-                    default:
-                        throw exception::LittleTilesException(exception::LittleTilesErrorCode::lt_unknow_angle);
-                }
-            }
-        };
-
-        using Boxes = GALIB_STD vector<Box>;
-
-        struct BlockTiles {
-            using const_iterator = GALIB_STD map<GALIB_STD string, Boxes>::const_iterator;
-            // <block_id, array of box>
-            using container = GALIB_STD map<GALIB_STD string, Boxes>;
-
-            GALIB minecraft::BlockCoordinate block_coordinate;
-            GridType grid;
-            GALIB_STD string little_tiles_id;
-            container boxes_map;
-        };
-
-        using const_iterator = GALIB_STD vector<BlockTiles>::const_iterator;
-        using container = GALIB_STD vector<BlockTiles>;
+        using const_iterator = GALIB_STD map<GALIB_STD string, BoxTileEnities>::const_iterator;
+        // <block_id, array of box>
+        using container = GALIB_STD map<GALIB_STD string, BoxTileEnities>;
 
     public:
-        ChunkTiles();
-        ~ChunkTiles()=default;
+        BlockTileEntities();
+        ~BlockTileEntities()=default;
 
     public:
-        bool ReadChunk(const GALIB minecraft::AnvilReader::ChunkDataReference& kChunkDataReference);
+        void readBlockTileNBT(const GALIB_NBT tag_compound& kBlockTilesNBT);
+
+        GALIB_NODISCARD const GALIB minecraft::BlockCoordinate& getBlockCoordinate()const;
+        GALIB_NODISCARD const GridType& getGridType()const;
+        GALIB_NODISCARD const GALIB_STD string& getLittleTilesID()const;
+
+        GALIB_NODISCARD const_iterator cbegin()const;
+        GALIB_NODISCARD const_iterator cend()const;
+
+    private:
+        GALIB minecraft::BlockCoordinate block_coordinate_;
+        GALIB_STD string little_tiles_id_;
+        GridType grid_;
+        container box_tile_enities_map_;
+    };
+
+    class ChunkTileEntities {
+    public:
+        using const_iterator = GALIB_STD vector<BlockTileEntities>::const_iterator;
+        using container = GALIB_STD vector<BlockTileEntities>;
+
+    public:
+        ChunkTileEntities();
+        ~ChunkTileEntities()=default;
+
+    public:
+        void ReadChunk(const GALIB minecraft::AnvilReader::ChunkDataReference& kChunkDataReference);
         GALIB_NODISCARD const GALIB minecraft::ChunkCoordinate& GetChunkCoordinate()const;
 
         GALIB_NODISCARD const_iterator cbegin()const;
@@ -151,7 +162,7 @@ namespace galib::minecraft::littletiles{
 
     private:
         GALIB minecraft::ChunkCoordinate chunk_coordinate_;
-        container block_tileses_;
+        container block_tile_entities_;
     };
 }
 
