619801e362c181ec70aa629468ee9ce97782051c
Gaksy <gaksys@outlook.com>
2025-04-27 17:01:30 +0800

    add getEnvVar function to get region folder for develop env

--- main.cpp
+++ main.cpp
@@ -10,31 +10,34 @@ using galib::minecraft::ChunkCoordinate;
 
 using galib::minecraft::littletiles::ChunkTileEntities;
 
-std::optional<std::string> get_region_folder() {
-    const char* folder = std::getenv("REGION_FOLDER");
-    if (folder == nullptr) {
-        return std::nullopt;
+std::string getEnvVar(const std::string& varName) {
+    char* value = nullptr;
+    size_t size = 0;
+    // Using _dupenv_s for safer environment variable retrieval
+    if (_dupenv_s(&value, &size, varName.c_str()) == 0 && value != nullptr) {
+        std::string result(value);
+        free(value);  // Don't forget to free the allocated memory
+        return result;
     }
-    return std::string(folder);
+    return "";  // Return empty string if environment variable is not found
 }
-
 
 int main() {
     printf("Hello, There is LittleTile Reader\n");
-    auto region_folder = get_region_folder();
+    std::string region_folder = getEnvVar("REGION_FOLDER");
 
-    if (!region_folder) {
+    if (region_folder.empty()) {
         printf("Env \"REGION_FOLDER\" Not found.\n");
         return EXIT_FAILURE;
     }
     else {
-        printf("region folder path is %s\n", region_folder.value().c_str());
+        printf("region folder path is %s\n", region_folder.c_str());
     }
 
     AnvilReader anvil_reader;
     constexpr ChunkCoordinate chunk_coord = {-1, 0};
 
-    anvil_reader.setRegionFolder(region_folder.value().c_str());
+    anvil_reader.setRegionFolder(region_folder.c_str());
     const AnvilReader::ChunkDataReference chunk_data_reference = anvil_reader.GetChunkDataReference(chunk_coord);
 
     ChunkTileEntities chunk_tile_manager;
