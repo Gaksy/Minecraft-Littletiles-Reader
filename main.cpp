40272b16d378c1e02fcc2ff3c231b62cd6dcf5ae
Gaksy <gaksys@outlook.com>
2025-04-27 16:40:44 +0800

    update LittleTiles

--- main.cpp
+++ main.cpp
@@ -1,4 +1,7 @@
 #include <cstdlib>
+#include <string>
+#include <optional>
+
 #include "Minecraft/Anvil.h"
 #include "Minecraft/LittleTiles.h"
 
@@ -7,13 +10,31 @@ using galib::minecraft::ChunkCoordinate;
 
 using galib::minecraft::littletiles::ChunkTileEntities;
 
+std::optional<std::string> get_region_folder() {
+    const char* folder = std::getenv("REGION_FOLDER");
+    if (folder == nullptr) {
+        return std::nullopt;
+    }
+    return std::string(folder);
+}
+
+
 int main() {
-    printf("Hello, There is LittleTile Reader");
+    printf("Hello, There is LittleTile Reader\n");
+    auto region_folder = get_region_folder();
+
+    if (!region_folder) {
+        printf("Env \"REGION_FOLDER\" Not found.\n");
+        return EXIT_FAILURE;
+    }
+    else {
+        printf("region folder path is %s\n", region_folder.value().c_str());
+    }
 
     AnvilReader anvil_reader;
     constexpr ChunkCoordinate chunk_coord = {-1, 0};
 
-    anvil_reader.setRegionFolder(R"(D:\Development\Minecaft\MinecraftLittletilesReader\region)");
+    anvil_reader.setRegionFolder(region_folder.value().c_str());
     const AnvilReader::ChunkDataReference chunk_data_reference = anvil_reader.GetChunkDataReference(chunk_coord);
 
     ChunkTileEntities chunk_tile_manager;
