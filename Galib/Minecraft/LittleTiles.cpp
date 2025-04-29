2720c260cb00498bf9fefa4147f0b89292c91583
Gaksy <gaksys@outlook.com>
2025-04-29 20:09:28 +0800

    Add CGAL littletiles builder

---
+++ Galib/Minecraft/LittleTiles.cpp
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2024 Gaksy (Fuhongren)
+ * 
+ * This work is licensed under the GNU Lesser General Public License v3.0.
+ * You may obtain a copy of the license at https://www.gnu.org/licenses/lgpl-3.0.html.
+ *
+ * This source code form is subject to the terms of the LGPL v3.0 license.
+ * If a copy of the LGPL was not distributed with this file, you can obtain one
+ * at the above license URL.
+ */
+ 
+/*
+ * Author: Gaksy
+ * Date Created: 04/29/2025
+ */
+
+#include <cinttypes>
+
+#include "GalibNamespaceDef.h"
+#include "Minecraft/LittleTiles.h"
+
+using GALIB minecraft::littletiles::AngleID;
+using GALIB_STD uint8_t;
+
+uint8_t (GALIB minecraft::littletiles::convertkAngleIdToInt)(const AngleID kAngleId) {
+    return static_cast<uint8_t>(kAngleId);
+}
+
+AngleID (galib::minecraft::littletiles::convertIntToAngleId)(const uint8_t kNumId) {
+    return static_cast<AngleID>(kNumId);
+}
