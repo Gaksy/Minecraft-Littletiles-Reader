7f31ee777a29369385f41e4b057c147b381fc61b
Gaksy <gaksys@outlook.com>
2025-05-13 15:23:25 +0800

    添加角的偏移示例

--- matlab/script/tile.m
+++ matlab/script/tile.m
@@ -1,20 +1,12 @@
 % 定义网格大小
-grid_type = 8;
+grid_type = 4;
 
 % 定义立方体的八个顶点
-dirt_box = LtBlock(3,1,3,5,3,5);
-clay_1 = LtBlock(3,0,2,5,1,6);
-clay_2 = LtBlock(5,0,3,6,4,5);
-clay_3 = LtBlock(2,0,3,3,4,5);
-clay_4 = LtBlock(3,1,5,5,4,6);
-clay_5 = LtBlock(3,1,2,5,4,3);
-
+block = LtBlock(1,1,1,3,3,3);
+block.EUS.y = 1;
+block.EUN.y = 1;
 % 绘制
 showGrid(grid_type, 0);
-patchLtBlock(dirt_box, 'green', 0.8);
-patchLtBlock(clay_1, 'cyan', 0.8);
-patchLtBlock(clay_2, 'cyan', 0.8);
-patchLtBlock(clay_3, 'cyan', 0.8);
-patchLtBlock(clay_4, 'cyan', 0.8);
-patchLtBlock(clay_5, 'cyan', 0.8);
+patchLtBlock(block, 'green', 0.7);
+
 %cyan
