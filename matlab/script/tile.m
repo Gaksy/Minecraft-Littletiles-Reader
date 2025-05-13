59a8de4f1522ea7ee7f4f21a86175b3750489d96
Gaksy <gaksys@outlook.com>
2025-05-13 12:22:45 +0800

    更新花盆示例和NBT结构表示

--- matlab/script/tile.m
+++ matlab/script/tile.m
@@ -4,10 +4,10 @@ grid_type = 8;
 % 定义立方体的八个顶点
 dirt_box = LtBlock(3,1,3,5,3,5);
 clay_1 = LtBlock(3,0,2,5,1,6);
-clay_2 = LtBlock(2,0,3,3,4,5);
-clay_3 = LtBlock(5,0,3,6,4,5);
-clay_4 = LtBlock(3,1,2,5,4,3);
-clay_5 = LtBlock(3,1,5,5,4,6);
+clay_2 = LtBlock(5,0,3,6,4,5);
+clay_3 = LtBlock(2,0,3,3,4,5);
+clay_4 = LtBlock(3,1,5,5,4,6);
+clay_5 = LtBlock(3,1,2,5,4,3);
 
 % 绘制
 showGrid(grid_type, 0);
