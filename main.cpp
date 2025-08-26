c5ccab513543e89cc8255fb412cca29a64d807f4
Gaksy <gaksys@outlook.com>
2025-08-26 23:19:12 +0800

    删除 EasyX 支持，添加Mac支持

--- main.cpp
+++ main.cpp
@@ -1,36 +1,3 @@
-
-#include <EasyXSupport/Button.h>
-#include <EasyXSupport/WidgetManager.h>
-#undef HAS_EASYX
-#ifdef HAS_EASYX
-#include <cstdlib>
-#include <cstdio>
-#include <graphics.h>
-#include <cinttypes>
-
-#include "Minecraft/MinecraftCoord.h"
-
-int main() {
-    printf("EasyX Version: %s\n", GetEasyXVer());
-    galib::easy_x::WidgetManager window(600, 600);
-
-    galib::easy_x::Button button(&window);
-
-    button.setWidgetSize(120,40);
-    button.setDisplayPos(10, 10);
-    button.setLabel("Hello World");
-
-    galib::easy_x::Button button_2(&window);
-
-    button_2.setWidgetSize(120,40);
-    button_2.setDisplayPos(10, 55);
-    button_2.setLabel("WTF");
-
-    window.start();
-    return EXIT_SUCCESS;
-}
-
-#else
 #include <cstdlib>
 #include <string>
 #include <optional>
@@ -43,40 +10,39 @@ using galib::minecraft::ChunkCoordinate;
 
 using galib::minecraft::littletiles::ChunkTileEntities;
 
-std::string getEnvVar(const std::string& varName) {
-    char* value = nullptr;
-    size_t size = 0;
-    // Using _dupenv_s for safer environment variable retrieval
-    if (_dupenv_s(&value, &size, varName.c_str()) == 0 && value != nullptr) {
-        std::string result(value);
-        free(value);  // Don't forget to free the allocated memory
-        return result;
-    }
-    return "";  // Return empty string if environment variable is not found
-}
+// std::string getEnvVar(const std::string& varName) {
+//     char* value = nullptr;
+//     size_t size = 0;
+//     // Using _dupenv_s for safer environment variable retrieval
+//     if (_dupenv_s(&value, &size, varName.c_str()) == 0 && value != nullptr) {
+//         std::string result(value);
+//         free(value);  // Don't forget to free the allocated memory
+//         return result;
+//     }
+//     return "";  // Return empty string if environment variable is not found
+// }
 
 int main() {
-    printf("Hello, There is LittleTile Reader\n");
-    std::string region_folder = getEnvVar("REGION_FOLDER");
-    // std::string region_folder = "D:\\Minecraft\\PLCII\\.minecraft\\versions\\Lt\\saves\\test\\region";
-
-    if (region_folder.empty()) {
-        printf("Env \"REGION_FOLDER\" Not found.\n");
-        return EXIT_FAILURE;
-    }
-    else {
-        printf("region folder path is %s\n", region_folder.c_str());
-    }
-
-    AnvilReader anvil_reader;
-    constexpr ChunkCoordinate chunk_coord = {0, 0};
-
-    anvil_reader.setRegionFolder(region_folder.c_str());
-    const AnvilReader::ChunkDataReference chunk_data_reference = anvil_reader.getChunkDataReference(chunk_coord);
-
-    ChunkTileEntities chunk_tile_manager;
-    chunk_tile_manager.readChunk(chunk_data_reference);
+    // printf("Hello, There is LittleTile Reader\n");
+    // std::string region_folder = getEnvVar("REGION_FOLDER");
+    // // std::string region_folder = "D:\\Minecraft\\PLCII\\.minecraft\\versions\\Lt\\saves\\test\\region";
+    //
+    // if (region_folder.empty()) {
+    //     printf("Env \"REGION_FOLDER\" Not found.\n");
+    //     return EXIT_FAILURE;
+    // }
+    // else {
+    //     printf("region folder path is %s\n", region_folder.c_str());
+    // }
+    //
+    // AnvilReader anvil_reader;
+    // constexpr ChunkCoordinate chunk_coord = {0, 0};
+    //
+    // anvil_reader.setRegionFolder(region_folder.c_str());
+    // const AnvilReader::ChunkDataReference chunk_data_reference = anvil_reader.getChunkDataReference(chunk_coord);
+    //
+    // ChunkTileEntities chunk_tile_manager;
+    // chunk_tile_manager.readChunk(chunk_data_reference);
 
     return EXIT_SUCCESS;
 }
-#endif
