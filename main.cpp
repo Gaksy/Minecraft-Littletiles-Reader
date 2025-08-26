74a3a9f37d7c31a6de678a7cd8fa154db65c5c9e
Gaksy <gaksys@outlook.com>
2025-08-26 23:29:45 +0800

    定向到测试region

--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
 #include <cstdlib>
 #include <string>
-#include <optional>
 
 #include "Minecraft/Anvil.h"
 #include "Minecraft/LittleTiles.h"
@@ -10,39 +9,29 @@ using galib::minecraft::ChunkCoordinate;
 
 using galib::minecraft::littletiles::ChunkTileEntities;
 
-// std::string getEnvVar(const std::string& varName) {
-//     char* value = nullptr;
-//     size_t size = 0;
-//     // Using _dupenv_s for safer environment variable retrieval
-//     if (_dupenv_s(&value, &size, varName.c_str()) == 0 && value != nullptr) {
-//         std::string result(value);
-//         free(value);  // Don't forget to free the allocated memory
-//         return result;
-//     }
-//     return "";  // Return empty string if environment variable is not found
-// }
+#if WIN32
+#define REGION_FOLDER "../test_region"
+#elif __APPLE__
+#define REGION_FOLDER "../test_region"
+#endif
+
 
 int main() {
-    // printf("Hello, There is LittleTile Reader\n");
-    // std::string region_folder = getEnvVar("REGION_FOLDER");
-    // // std::string region_folder = "D:\\Minecraft\\PLCII\\.minecraft\\versions\\Lt\\saves\\test\\region";
-    //
-    // if (region_folder.empty()) {
-    //     printf("Env \"REGION_FOLDER\" Not found.\n");
-    //     return EXIT_FAILURE;
-    // }
-    // else {
-    //     printf("region folder path is %s\n", region_folder.c_str());
-    // }
-    //
-    // AnvilReader anvil_reader;
-    // constexpr ChunkCoordinate chunk_coord = {0, 0};
-    //
-    // anvil_reader.setRegionFolder(region_folder.c_str());
-    // const AnvilReader::ChunkDataReference chunk_data_reference = anvil_reader.getChunkDataReference(chunk_coord);
-    //
-    // ChunkTileEntities chunk_tile_manager;
-    // chunk_tile_manager.readChunk(chunk_data_reference);
+    printf("Hello, There is LittleTile Reader\n");
+
+    const std::string region_folder = REGION_FOLDER;
+    printf("region folder path is %s\n", region_folder.c_str());
+
+    AnvilReader anvil_reader;
+    constexpr ChunkCoordinate chunk_coord = {0, 0};
+
+    anvil_reader.setRegionFolder(region_folder.c_str());
+    const AnvilReader::ChunkDataReference chunk_data_reference = anvil_reader.getChunkDataReference(chunk_coord);
+
+    ChunkTileEntities chunk_tile_manager;
+    chunk_tile_manager.readChunk(chunk_data_reference);
+
+
 
     return EXIT_SUCCESS;
 }
